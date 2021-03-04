99cffd2d10e00872d322a2794914e2bbe04a1dff
radhesaini <radhe830222@gmail.com>
2021-03-04 16:40:21 +0530

    make code standered using pylint

--- my code/math.py
+++ my code/math.py
@@ -1,29 +1,39 @@
+"""math module
+"""
 from typing import Optional
 
 from fastapi import FastAPI
 
-app = FastAPI()
+APP = FastAPI()
 
 
-@app.get("/")
+
+@APP.get("/")
 def read_root():
+    """
+    localhost output"""
     return {"Hello": "World"}
 
 
-@app.get("/fibonacci/{length}")
+
+@APP.get("/fibonacci/{length}")
 def fibonacci(length: int, q: Optional[str] = None):
-    fibonacci_searis=[0,1]
-    if length<2:
+    """fibonacci module
+    """
+    fibonacci_searis = [0, 1]
+    if length < 2:
         return fibonacci_searis[0]
-    elif length==2:
+    if length == 2:
         return fibonacci_searis[1]
-    for index in range(2,length,1):
-        fibonacci_searis+=[fibonacci_searis[index-1]+ar[index-2]]
+    for index in range(2, length, 1):
+        fibonacci_searis += [fibonacci_searis[index-1]+fibonacci_searis[index-2]]
     return {"fibonacci item": fibonacci_searis[length-1], "q": q}
 
-@app.get("/factorial/{number}")
+
+@APP.get("/factorial/{number}")
 def factorial(number: int, q: Optional[str] = None):
-    if number<=1:
+    """factorial module
+    """
+    if number <= 1:
         return 1
     return {"result factorial":number*factorial(number-1), "q": q}
-
--- my code/string.py
+++ my code/string.py
@@ -1,28 +1,37 @@
+"""string module
+"""
 from typing import Optional
 
 from fastapi import FastAPI
 
-app = FastAPI()
+APP = FastAPI()
 
 
-@app.get("/")
+
+@APP.get("/")
 def read_root():
+    """
+    localhost output"""
     return {"Hello": "World"}
 
 
-@app.get("/length/{sample_string}")
+
+@APP.get("/length/{sample_string}")
 def length(sample_string: str, q: Optional[str] = None):
-    index=0
+    """length function"""
+    index = 0
     try:
         while sample_string[index]:
-            index+=1
-    except IndexErrorr:
-        return {"length of string": index, "q": q}
+            index += 1
+    except IndexError:
+        return {"length of string": index, "Q": q}
 
-@app.get("/gen_string/{no_of_char}")
+
+@APP.get("/gen_string/{no_of_char}")
 def gen_string(no_of_char: int, q: Optional[str] = None):
-    result_string=''
+    """
+    2 nd end point - string generation"""
+    result_string = ''
     for _ in range(no_of_char):
-        result_string+=input()
+        result_string += input()
     return {"generated string":result_string, "q": q}
-
