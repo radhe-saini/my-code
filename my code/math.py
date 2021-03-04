dfa457ef502c45228dbf3b40f8a1d405a208f03e
radhesaini <radhe830222@gmail.com>
2021-03-04 15:40:11 +0530

    make readable code

--- my code/math.py
+++ my code/math.py
@@ -10,20 +10,20 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/fibonacci/{n}")
-def fibonacci(n: int, q: Optional[str] = None):
-    ar=[0,1]
-    if n<2:
-        return ar[0]
-    elif n==2:
-        return ar[1]
-    for i in range(2,n,1):
-        ar+=[ar[i-1]+ar[i-2]]
-    return {"item_id": ar[n-1], "q": q}
+@app.get("/fibonacci/{length}")
+def fibonacci(length: int, q: Optional[str] = None):
+    fibonacci_searis=[0,1]
+    if length<2:
+        return fibonacci_searis[0]
+    elif length==2:
+        return fibonacci_searis[1]
+    for index in range(2,length,1):
+        fibonacci_searis+=[fibonacci_searis[index-1]+ar[index-2]]
+    return {"fibonacci item": fibonacci_searis[length-1], "q": q}
 
-@app.get("/fectorial/{n}")
-def fectorial(n: int, q: Optional[str] = None):
-    if n<1:
+@app.get("/factorial/{number}")
+def factorial(number: int, q: Optional[str] = None):
+    if number<=1:
         return 1
-    return {"item_id":n*fectorial(n-1), "q": q}
+    return {"result factorial":number*factorial(number-1), "q": q}
 
--- my code/string.py
+++ my code/string.py
@@ -10,17 +10,19 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/length/{s}")
-def length(s: str, q: Optional[str] = None):
-    i=0
-    while s[i]:
-        i+=1
-    return {"item_id": i, "q": q}
+@app.get("/length/{sample_string}")
+def length(sample_string: str, q: Optional[str] = None):
+    index=0
+    try:
+        while sample_string[index]:
+            index+=1
+    except IndexErrorr:
+        return {"length of string": index, "q": q}
 
-@app.get("/gen_string/{n}")
-def gen_string(n: int, q: Optional[str] = None):
-    s=''
-    for i in range(n):
-        s+=input()
-    return {"item_id":s, "q": q}
+@app.get("/gen_string/{no_of_char}")
+def gen_string(no_of_char: int, q: Optional[str] = None):
+    result_string=''
+    for _ in range(no_of_char):
+        result_string+=input()
+    return {"generated string":result_string, "q": q}
 
